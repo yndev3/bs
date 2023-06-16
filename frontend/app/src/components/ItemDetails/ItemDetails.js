5499a1273aca5b6404fd5c6515b6a7178b768843
FOPEN2010 <92811839+FOPEN2010@users.noreply.github.com>
2023-06-16 16:23:11 +0900

    /item-details

--- frontend/app/src/components/ItemDetails/ItemDetails.js
+++ frontend/app/src/components/ItemDetails/ItemDetails.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/dist/css/themes/splide-default.min.css';
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+} from 'react-accessible-accordion';
+import 'react-accessible-accordion/dist/fancy-example.css';
 
 const initData = {
+    ContractAddress: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+    Category: "Watch",
     title: "Walking On AirWalking On AirWalking On Air",
-    itemImg: "/img/auction_2.jpg",
     price: "1,000",
-    ownerImg: "/img/avatar_1.jpg",
-    itemOwner: "Themeland",
+    image: "/img/Watches.jpg",
+    image_2: "/img/Jewelrys.jpg",
+    image_3: "/img/Materials.jpg",
+    image_4: "/img/auction_2.jpg",
+    image_5: "/img/auction_2.jpg",
+    image_6: "/img/auction_2.jpg",
+    image_7: "/img/auction_2.jpg",
+    image_8: "/img/auction_2.jpg",
+    image_9: "/img/auction_2.jpg",
+    image_10: "/img/auction_2.jpg",
+    image_11: "/img/auction_2.jpg",
+    image_12: "/img/Jewelrys.jpg",
+    image_13: "/img/Materials.jpg",
+    image_14: "/img/auction_2.jpg",
+    image_15: "/img/auction_2.jpg",
+    image_16: "/img/auction_2.jpg",
+    image_17: "/img/auction_2.jpg",
+    image_18: "/img/auction_2.jpg",
+    image_19: "/img/auction_2.jpg",
+    image_20: "/img/auction_2.jpg",
+    Brand: "ROLEX",
+    State: "Brand new",
+    Weight: "500",
+    Color: "Black/Silver",
+    Material: "ROLEX",
+    Size: "100*30*20",
+    Accessories: "Box, manual,Box, manual,Box",
     created: "15 Jul 2021",
     content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum obcaecati dignissimos quae quo ad iste ipsum officiis deleniti asperiores sit.",
     price_1: "1.5 ETH",
@@ -35,14 +71,27 @@ class ItemDetails extends Component {
     state = {
         initData: {},
         sellerData: []
-    }
-    componentDidMount(){
+    };
+
+    componentDidMount() {
         this.setState({
             initData: initData,
             sellerData: sellerData
-        })
+        });
     }
+
     render() {
+
+        {/* Required Loop*/}
+        const { initData } = this.state;
+        const requiredLoop = ["Brand", "State", "Weight", "Color", "Material", "Size", "Accessories"];
+        
+        {/* Contract address shortcut*/}
+        const address = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+        const maxLength = 10;
+        const truncatedAddress = `${address.slice(0, 10)}...${address.slice(-7)}`;
+        
+
         return (
             <section className="item-details-area">
                 <div className="container">
@@ -51,7 +100,18 @@ class ItemDetails extends Component {
                             <div className="item-info">
                                 <h3 className="mt-0">{this.state.initData.title}</h3>
                                 <div className="item-thumb text-center">
-                                    <img src={this.state.initData.itemImg} alt="" />
+                                    <Splide aria-label="itemImg" >
+                                        {Object.keys(this.state.initData).map((key) => {
+                                            if (key.startsWith('image')) {
+                                                return (
+                                                    <SplideSlide key={key}>
+                                                        <img src={this.state.initData[key]} alt='item_image'/>
+                                                    </SplideSlide>
+                                                );
+                                            }
+                                            return null;
+                                        })}
+                                    </Splide>
                                 </div>
                             </div>
                             <div className="col-12 item px-lg-2 mt-3">
@@ -65,60 +125,80 @@ class ItemDetails extends Component {
                             </div>
                             <a className="d-block btn btn-bordered-white mt-4" href="/wallet-connect">Place a Buy</a>
                         </div>
+
+                        {/* Right column*/}
                         <div className="col-12 col-lg-6">
                             {/* Content */}
                             <div className="content mt-5 mt-lg-0">
+                                {/* Description */}
                                 <p>
-                                    <span className='text-white'>Description</span><br/>
-                                    {this.state.initData.content}
+                                    <span className='text-white h5'>Description</span><br/>
+                                    <span className='h6'>{this.state.initData.content}</span>
                                 </p>
-                                {/* Owner */}
-                                <div className="owner d-flex align-items-center">
-                                    <span>Owned By</span>
-                                    <a className="owner-meta d-flex align-items-center ml-3" href="/author">
-                                        <img className="avatar-sm rounded-circle" src={this.state.initData.ownerImg} alt="" />
-                                        <h6 className="ml-2">{this.state.initData.itemOwner}</h6>
-                                    </a>
-                                </div>
-                                {/* Item Info List */}
+                                {/* Required List */}
                                 <div className="item-info-list mt-4">
                                     <ul className="list-unstyled">
-                                        <li className="price d-flex justify-content-between">
-                                            <span>Current Price {this.state.initData.price_1}</span>
-                                            <span>{this.state.initData.price_2}</span>
-                                            <span>{this.state.initData.count}</span>
+                                        {requiredLoop.map((key) => (
+                                        <li className="price d-flex justify-content-between" key={key}>
+                                        <span className='mr-3 text-white'>{key}</span>
+                                        <span className='word-break'>{initData[key]}</span>
                                         </li>
-                                        <li>
-                                            <span>Size </span>
-                                            <span>{this.state.initData.size}</span>
-                                        </li>
-                                        <li>
-                                            <span>Volume Traded </span>
-                                            <span>{this.state.initData.volume}</span>
-                                        </li>
+                                        ))}
                                     </ul>
                                 </div>
-                                <div className="row items">
-                                    {this.state.sellerData.map((item, idx) => {
-                                        return (
-                                            <div key={`sd_${idx}`} className="col-12 col-md-6 item px-lg-2">
-                                                <div className="card no-hover">
-                                                    <div className="single-seller d-flex align-items-center">
-                                                        <a href="/author">
-                                                            <img className="avatar-md rounded-circle" src={item.img} alt="" />
-                                                        </a>
-                                                        {/* Seller Info */}
-                                                        <div className="seller-info ml-3">
-                                                            <a className="seller mb-2" href="/author">{item.seller}</a>
-                                                            <span>{item.post}</span>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        );
-                                    })}
+                                {/* Item Info List */}
+                                <div className="accordion mt-5">
+                                <Accordion  allowZeroExpanded>
+                                    {/* Optional List */}
+                                    <AccordionItem>
+                                        <AccordionItemHeading>
+                                            <AccordionItemButton>
+                                                Optional
+                                            </AccordionItemButton>
+                                        </AccordionItemHeading>
+                                        <AccordionItemPanel>
+                                            <p>
+                                                Exercitation in fugiat est ut ad ea cupidatat ut in
+                                                cupidatat occaecat ut occaecat consequat est minim minim
+                                                esse tempor laborum consequat esse adipisicing eu
+                                                reprehenderit enim.
+                                            </p>
+                                        </AccordionItemPanel>
+                                    </AccordionItem>
+                                    {/* Details List */}
+                                    <AccordionItem>
+                                        <AccordionItemHeading>
+                                            <AccordionItemButton>
+                                                Details
+                                            </AccordionItemButton>
+                                        </AccordionItemHeading>
+                                        <AccordionItemPanel>
+                                        <p>   
+                                            <ul className="list-unstyled">
+                                                <li className="price d-flex justify-content-between">
+                                                    <span className='mr-3 text-white'>Contract Address</span>
+                                                    <span className='word-break'>
+                                                        <a href={`https://etherscan.io/address/${initData.ContractAddress}`} target='_blank'>{truncatedAddress}</a>
+                                                    </span>
+                                                </li>
+                                                <li className="price d-flex justify-content-between">
+                                                    <span className='mr-3 text-white'>Token ID</span>
+                                                    <span className='word-break'>
+                                                        <a href="https://ipfs.io/ipfs/QmeifaBHYmARgCDU2ZnzajKNEsAyAYgz67g25c9KkbcR5y/2657.json" target='_blank'>1234</a>
+                                                    </span>
+                                                </li>
+                                                <li className="price d-flex justify-content-between">
+                                                    <span className='mr-3 text-white'>Token Standard</span><span className='word-break'>ERC-721</span>
+                                                </li>
+                                                <li className="price d-flex justify-content-between">
+                                                    <span className='mr-3 text-white'>Chain</span><span className='word-break'>Polygon</span>
+                                                </li>
+                                            </ul>
+                                        </p>
+                                        </AccordionItemPanel>
+                                    </AccordionItem>
+                                </Accordion>
                                 </div>
-
                             </div>
                         </div>
                     </div>
