739125319788e8be275c53ad0be37e428a489057
Yu <you.nagamine.yn@gmail.com>
2023-06-26 15:40:16 +0900

    address record when deployed

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,31 @@
-const tokenURI = 'QmYjL1KJozAXQebydEmovja6LRXCDwjSwf81k3wLL1HDQi';
-const addr = "0xD38Eb334caC02650c1Dc01f6f98b78dbdFAC7A67";
+import fs from 'fs';
+
+const tokenURI = 'bafkreih3ybpzqlxcg25peckyyzd6iddoldpbrxl46dng7ggg4tovookwvq';
+const addr = '0xD38Eb334caC02650c1Dc01f6f98b78dbdFAC7A67';
 const main = async () => {
   let BrandSwap = await ethers.getContractFactory('BrandSwap');
   let brandSwap = await BrandSwap.deploy();
   await brandSwap.deployed();
-  console.log(`Contract deployed to: https://mumbai.polygonscan.com/address/${brandSwap.address}`);
+  console.log(
+      `Contract deployed to: https://mumbai.polygonscan.com/address/${ brandSwap.address }`);
 
   const tx = await brandSwap.nftMint(tokenURI);
   tx.wait();
-  console.log("NFT#1 minted...");
-}
+  console.log('NFT#1 minted...');
+
+  fs.writeFileSync(
+      './memberNFTContract.js',
+      `
+            module.exports = "${ brandSwap.address }"
+          `,
+  );
+};
 
 const deploy = async () => {
-  try{
+  try {
     await main();
     process.exit(0);
-  } catch(err) {
+  } catch (err) {
     console.log(err);
     process.exit(1);
   }
