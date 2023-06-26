0a7812a16910cc75476ce8cbb08e0080607e11ba
Yu <you.nagamine.yn@gmail.com>
2023-06-26 16:00:03 +0900

    check connect metamask and correct chain

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
 // importing MyRouts where we located all of our theme
-import MyRouts from './routers/routes'
+import MyRouts from './routers/routes';
 
-function App() {
+export default function App() {
+  const [chainId, setChainId] = useState(false);
+
+  const mumbaiId = '0x13881';
+  const zeroAddress = '0x0000000000000000000000000000000000000000';
+  const checkMetaMaskInstalled = async () => {
+    const {ethereum} = window;
+    if (!ethereum) {
+      alert('Please, Install Meta Mask!');
+    }
+  };
+
+  const checkChainId = async () => {
+    const {ethereum} = window;
+    if (ethereum) {
+      const chain = await ethereum.request({
+        method: 'eth_chainId',
+      });
+      console.log(`chain: ${ chain }`);
+
+      if (chain !== mumbaiId) {
+        alert('Connect with Mumbai Testnet!');
+        setChainId(false);
+      } else {
+        setChainId(true);
+      }
+    }
+  };
+
+  useEffect(() => {
+    checkMetaMaskInstalled();
+    checkChainId();
+  }, []);
   return (
-    <div>
-      <MyRouts />
-    </div>
+      <div>
+        <MyRouts/>
+      </div>
   );
 }
-
-export default App;
