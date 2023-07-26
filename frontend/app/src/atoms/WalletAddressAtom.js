108084e80de2c1bbe2f26782630127855aba8e63
Yu <you.nagamine.yn@gmail.com>
2023-07-26 10:02:23 +0900

    add gitignore

--- frontend/app/src/atoms/WalletAddressAtom.js
+++
@@ -1,9 +0,0 @@
-import { atom } from "recoil";
-import { recoilPersist } from"recoil-persist";
-
-const { persistAtom } = recoilPersist()
-export const walletAddressAtom = atom({
-  key: 'walletAddressAtom',
-  default: null,
-  effects_UNSTABLE: [persistAtom],
-});
