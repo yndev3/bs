29f58f69a3537f72324fd9f8664b4d9c4883bd31
Yu <mrbaron333+dev@gmail.com>
2023-09-05 12:32:05 +0900

    create endpoint

--- backend/app/routes/api.php
+++ backend/app/routes/api.php
@@ -19,10 +19,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-//
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
 // user only
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user-nft-list', [UserController::class, 'fetchUserNFTList']);
@@ -37,7 +33,3 @@ Route::get('/item-limit', [ItemListController::class, 'withLimit']);
 Route::get('/statement', [AuthController::class, 'getStatement']);
 Route::post('/login'   , [AuthController::class, 'login']);
 Route::post('/logout'   , [AuthController::class, 'logout']);
-
-
-
-
