95ab9d1fe7e05366bdb6455345c698530caceea8
Yu <mrbaron333+dev@gmail.com>
2023-09-05 11:01:11 +0900

    add user_product seeder

--- backend/app/database/seeders/DatabaseSeeder.php
+++ backend/app/database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ProductSeeder::class,
             UserSeeder::class,
+            UserProductSeeder::class,
         ]);
     }
 }
