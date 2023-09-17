0664668031a1ced0e3e652398a3bce1716b472cb
FOPEN2010 <92811839+FOPEN2010@users.noreply.github.com>
2023-09-18 01:15:26 +0900

    top card detail css FRONT

--- frontend/src/components/Item/Card.jsx
+++ frontend/src/components/Item/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Card = (props) => {
   const item = props.item;
@@ -12,36 +13,42 @@ const Card = (props) => {
     }
   }
 
+  const formatPrice = (price) => {
+    return price.toLocaleString('en-US', { minimumFractionDigits: 0, maximumFractionDigits: 0 });
+  };
+
   return (
       <>
         <div className="mt-3 col-12 col-sm-6 col-lg-3">
-          <div className="card">
+          <div className="card card-min">
             <div className="image-over">
-              <a href={ `/item-details/${ item.id }` }>
+              <Link to={ `/item-details/${ item.id }` }>
                 <img className="card-img-top"
                      src={ convertIpfsLink(item.image) } alt=""/>
-              </a>
+              </Link>
             </div>
             {/* Card Caption */ }
             <div className="card-caption col-12 p-0">
               {/* Card Body */ }
               <div className="card-body">
                 <h5 className="mb-0">{ item.name }</h5>
-                <div className="seller align-items-center my-3">
+                <div className="seller align-items-center my-3 mb-3">
                   <span>Brand by</span>
-                  <h6 className="mb-0">{ item.brand }</h6>
+                  <h6 className="mb-0 ellipsis">{ item.brand }</h6>
                 </div>
                 <div className="card-bottom d-flex justify-content-between">
-                    <span>{item.price} USDT</span>
                     <span>1 of 1</span>
+                    <span>
+                      <img className="mr-2" src="../img/tether-usdt-logo.png" alt="usdtlogo" width="21px"/>
+                      <span className="card-price">{formatPrice(item.price)}</span> USDT</span>
                 </div>
-                <div className="col-12 text-center mt-2">
+                <div className="col-12 text-center mt-3">
 
-                  <a className="btn btn-bordered-white btn-smaller mt-3"
-                     href={ `/item-details/${ item.id }` }>
-                      <i className="fa fa-arrow-circle-right mr-2" aria-hidden="true"></i>
+                  <Link className="btn btn-bordered-white btn-smaller mt-3"
+                     to={ `/item-details/${ item.id }` }>
+                      <i className="icon-handbag mr-2" aria-hidden="true"></i>
                      ItemDetail
-                  </a>
+                  </Link>
                 </div>
               </div>
             </div>
