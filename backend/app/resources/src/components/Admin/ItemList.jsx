be538cdc437f0dd8d89fd8db9c8ff5d47de67dc4
FOPEN2010 <92811839+FOPEN2010@users.noreply.github.com>
2023-09-20 01:51:05 +0900

    history.push('/error');
    APIエラー忘れ

--- backend/app/resources/src/components/Admin/ItemList.jsx
+++ backend/app/resources/src/components/Admin/ItemList.jsx
@@ -180,6 +180,7 @@ export default function StickyHeadTable() {
       })
       .catch((error) => {
         console.error('Error fetching data:', error);
+        history.push('/error');
       });
   }, []);
 
