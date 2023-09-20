f705977b6abd42ca7007f5bf42e4a12b98301e2a
FOPEN2010 <92811839+FOPEN2010@users.noreply.github.com>
2023-09-20 21:58:57 +0900

    root

--- frontend/src/components/Login/Redirect.jsx
+++ frontend/src/components/Login/Redirect.jsx
@@ -1,22 +1,22 @@
-import React, { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
 import { useAccount } from 'wagmi';
 
-const RedirectIfNotConnected = ({ children }) => {
+const PrivateRoute = ({ component: Component, ...rest }) => {
   const { isConnected } = useAccount();
-  const history = useHistory();
 
-  useEffect(() => {
-    if (!isConnected) {
-      history.push('/wallet-connect');
-    }
-  }, [isConnected, history]);
-
-  if (!isConnected) {
-    return null; 
-  }
-
-  return <>{children}</>;
+    return (
+        <Route 
+            {...rest} 
+            render={props => 
+              isConnected ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect to="/wallet-connect" /> 
+                )
+            }
+        />
+    );
 };
 
-export default RedirectIfNotConnected;
+export default PrivateRoute;
--- frontend/src/routers/routes.jsx
+++ frontend/src/routers/routes.jsx
@@ -15,29 +15,29 @@ import Account from '../themes/account';
 import Error from '../themes/error';
 import Error404 from '../themes/404';
 
-import RedirectIfNotConnected from '../components/Login/Redirect';
+import PrivateRoute from '../components/Login/Redirect';
 
 
 const MyRouts = () => {
   return (
       <>
-        <Router>
+      <Router>
           <ScrollToTop />
           <Switch>
-            <Route exact path="/" component={ Home }/>
-            <Route exact path="/explore-watches" component={ ExploreWatches }/>
-            <Route exact path="/explore-jewelrys" component={ ExploreJewelrys }/>
-            <Route exact path="/explore-materials" component={ ExploreMaterials }/>
-            <Route exact path="/item-details/:id" component={ ItemDetails }/>
-            <Route exact path="/item-details" component={ ItemDetails }/>
-            <Route exact path="/wallet-connect" component={ WalletConnect }/>
-            <Route exact path="/contact" component={ Contact }/>
-            <Route exact path="/error" component={ Error }/>
-            <Route exact path="/account" render={() => <RedirectIfNotConnected><Account /></RedirectIfNotConnected>} />
-            <Route exact path="/activity" render={() => <RedirectIfNotConnected><Activity /></RedirectIfNotConnected>} />
-            <Route component={ Error404 }/>
+              <Route exact path="/" component={ Home }/>
+              <Route exact path="/explore-watches" component={ ExploreWatches }/>
+              <Route exact path="/explore-jewelrys" component={ ExploreJewelrys }/>
+              <Route exact path="/explore-materials" component={ ExploreMaterials }/>
+              <Route exact path="/item-details/:id" component={ ItemDetails }/>
+              <Route exact path="/item-details" component={ ItemDetails }/>
+              <Route exact path="/wallet-connect" component={ WalletConnect }/>
+              <Route exact path="/contact" component={ Contact }/>
+              <Route exact path="/error" component={ Error }/>
+              <PrivateRoute exact path="/activity" component={ Activity }/>
+              <PrivateRoute exact path="/account" component={ Account }/>
+              <Route component={ Error404 }/>
           </Switch>
-        </Router>
+      </Router>
       </>
   );
 };
