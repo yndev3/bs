59cee6003f9877da5289b5d23900d71feaa75427
Yu <mrbaron333+dev@gmail.com>
2023-09-22 15:22:34 +0900

    add provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 // importing MyRouts where we located all of our theme
 import MyRouts from './routers/routes';
-import { useAccount, useDisconnect } from 'wagmi';
+import { useAccount, useDisconnect, useNetwork } from 'wagmi';
 import { useEffect } from 'react';
 import { logout } from './utils/fetchFromApi';
+import { useAuth } from './providers/AuthProvider';
 
 const App = () => {
+  const { setIsAuthenticated } = useAuth();
   const {disconnect} = useDisconnect();
+  const {chain, chains} = useNetwork();
   const {connector: activeConnector} = useAccount({
     onDisconnect() {
-      logout().then(() => {console.log('Disconnected');});
+      logout().then(() => {
+        setIsAuthenticated(false);
+        console.log('Disconnected');
+      });
     },
   });
   useEffect(() => {
