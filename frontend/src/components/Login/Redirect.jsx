332ff0f530571fe811830dedda6fe663fbeaf454
FOPEN2010 <92811839+FOPEN2010@users.noreply.github.com>
2023-09-26 18:04:29 +0900

    Update Redirect.jsx

--- frontend/src/components/Login/Redirect.jsx
+++ frontend/src/components/Login/Redirect.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { useAuth } from '../../providers/AuthProvider';
 import { useAccount } from 'wagmi';
-import { useAuth } from './providers/AuthProvider';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
     const { isConnected } = useAccount();
