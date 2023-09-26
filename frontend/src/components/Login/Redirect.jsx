bd7bb65be22406af271a2e2ad84b836cbd9b8013
FOPEN2010 <92811839+FOPEN2010@users.noreply.github.com>
2023-09-26 10:12:11 +0900

    Redirect.jsx

--- frontend/src/components/Login/Redirect.jsx
+++ frontend/src/components/Login/Redirect.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { useAuth } from '../../providers/AuthProvider';
-import { useDisconnect } from 'wagmi';
+import { useAccount } from 'wagmi';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
 
     const { isAuthenticated } = useAuth();
-    const { disconnect } = useDisconnect();
+    const { isConnected } = useAccount();
     
     return (
         <Route 
             {...rest} 
             render={props => 
-                isAuthenticated ? (
+                isAuthenticated && isConnected ? ( 
                     <Component {...props} />
                 ) : (
-                    <>
-                        {disconnect()}
-                        <Redirect to="/wallet-connect" /> 
-                    </>
+                    <Redirect to="/wallet-connect" /> 
                 )
             }
         />
